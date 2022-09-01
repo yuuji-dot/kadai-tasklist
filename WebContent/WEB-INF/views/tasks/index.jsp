59fa0ba81cfa6743e94fea4ebf77c7bfbc84252f
kosar <kosar@LAPTOP-140PAQBL>
2022-09-02 04:52:16 +0900

    バリデーションなどを追加

--- WebContent/WEB-INF/views/tasks/index.jsp
+++ WebContent/WEB-INF/views/tasks/index.jsp
@@ -21,10 +21,10 @@
 
         <div id="pagination">
             (全 ${tasks_count} 件)<br />
-           <c:forEach var="i" begin="1" end="${((tasks_count - 1) / 15) }">
+           <c:forEach var="i" begin="1" end="${((tasks_count - 1) / 15) +1}" step="1">
                 <c:choose>
-                    <c:when test="${ i == page}">
-                        <c:out value="${i}"/>&nbsp
+                    <c:when test="${i == page}">
+                        <c:out value="${i}"/>&nbsp;
                     </c:when>
                     <c:otherwise>
                         <a href="${pageContext.request.contextPath}/index?page=${i}"><c:out value="${i}"/></a>&nbsp;
--- src/controllers/UpdateServlet.java
+++ src/controllers/UpdateServlet.java
@@ -2,8 +2,10 @@ package controllers;
 
 import java.io.IOException;
 import java.sql.Timestamp;
+import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,6 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.Task;
+import models.validators.TaskValidator;
 import utils.DBUtil;
 
 /**
@@ -46,6 +49,19 @@ public class UpdateServlet extends HttpServlet {
             Timestamp currentTime = new Timestamp(System.currentTimeMillis());
             t.setUpdated_at(currentTime);       // 更新日時のみ上書き
 
+            //バリエーションを実行してエラーがあったら編集画面のフォームに戻る
+            List<String> errors = TaskValidator.validate(t);
+            if(errors.size() > 0) {
+                em.close();
+
+                //フォームに初期値を設定、さらにエラーメッセージを送る
+                request.setAttribute("_token", request.getSession().getId());
+                request.setAttribute("task",t);
+                request.setAttribute("errors", errors);
+
+                RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/edit.jsp");
+                rd.forward(request, response);
+            }else {
             //データベースを更新
             em.getTransaction().begin();
             em.getTransaction().commit();
@@ -57,6 +73,7 @@ public class UpdateServlet extends HttpServlet {
 
             //indexページへリダイレクト
             response.sendRedirect(request.getContextPath() + "/index");
+            }
         }
     }
 
